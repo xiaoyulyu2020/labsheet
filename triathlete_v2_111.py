e7c10292a3145a492c78f5296c6a7105dd077d4d
Xiaoyu Lyu <75165936+xiaoyulyu2020@users.noreply.github.com>
2021-04-07 14:19:00 +0100

    Add files via upload

---
+++ triathlete_v2_111.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+class Triathlete(object):
+   def __init__(self, name, tid):
+      self.name = name
+      self.tid = tid
+      self.dic = {}
+
+   def add_time(self, sport, time):
+      if sport not in self.dic:
+         self.dic[sport] = time
+         return
+
+   def get_time(self, sport):
+      return self.dic[sport]
+
+   def get_total(self, dic):
+      return sum(self.dic.values())
+
+   def __str__(self):
+      return f"Name: {self.name}\nID: {self.tid}\nRace time: {self.get_total(self.dic)}"
