dd3845a5fb3b19a9f036447f2d2a303a9bebdde2
Xiaoyu Lyu <75165936+xiaoyulyu2020@users.noreply.github.com>
2021-04-07 15:34:19 +0100

    Add files via upload

---
+++ triathlon_v2_111.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+class Triathlete(object):
+   def __init__(self, name, tid):
+      self.name = name
+      self.tid = tid
+
+   def __str__(self):
+      return f"Name: {self.name}\nID: {self.tid}"
+
+class Triathlon(object):
+   def __init__(self):
+      self.dic = {}
+
+   def add(self, other):
+      self.dic[other.name] = other.tid
+      return
+
+   def lookup(self, value):
+      for c in self.dic:
+         if self.dic[c] == value:
+            return Triathlete(c, value)
+         return None
+
+   def remove(self, value):
+      for c in self.dic:
+         if self.dic[c] == value:
+            del self.dic[c]
+            return self.dic
+
+   def __str__(self):
+      newline = "\n"
+      newdic = sorted(self.dic)
+      return f'{newline.join(f"Name: {key}{newline}ID: {self.dic[key]}" for key in newdic)}'
