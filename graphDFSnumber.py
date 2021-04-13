34e02a304cc9d53496e7fb46106839e98524f8ed
Xiaoyu Lyu <75165936+xiaoyulyu2020@users.noreply.github.com>
2021-04-13 23:15:41 +0100

    Add files via upload

---
+++ graphDFSnumber.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+class Graph(object):
+  def __init__(self, num):
+    self.num = num
+    self.adj = {}
+    for c in range(num):
+      self.adj[c] = list()
+  def addEdge(self, start, end):
+    self.adj[start] = end
+    self.adj[end] = start
+
+class DFSPaths(object):
+  def __init__(self, gra, start):
+    self.gra = gra
+    self.start = start
+    self.visited = [False for _ in range(gra.num)]
+    self.parent = [False for _ in range(gra.num)]
+    self.dfs(start)
+
+  def dfs(self, visit):
+    self.visited[visit] = True
+    for w in self.g.adj[visit]:
+      if not self.visited[w]:
+        self.parent[w] = visit
+        self.dfs(w)
+
+  def hasPathTo(self, end):
+    return self.visited[end]
+
+  def pathTo(self, end):
+    if not self.hasPathTo(end):
+      return None
+    path = [end]
+    while end != self.start:
+      end = self.parent[end]
+      path.append(end)
+    return path[::-1]
+
+
